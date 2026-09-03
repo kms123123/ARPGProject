94b40b2927346586adb6460a6d11c5d03ea198f4
kms123123 <1k2ms@yonsei.ac.kr>
2026-09-03 21:13:05 +0900

    캐릭터 파일 생성
    ARCharacterBase

---
+++ Source/ARPGProject/Character/ARCharacterBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ARCharacterBase.generated.h"
+
+UCLASS()
+class ARPGPROJECT_API AARCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AARCharacterBase();
+
+};
