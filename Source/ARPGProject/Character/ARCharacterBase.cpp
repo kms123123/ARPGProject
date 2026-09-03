b3ec8bb3e2495f4657d7e775831af148110edf37
kms123123 <1k2ms@yonsei.ac.kr>
2026-09-03 21:55:51 +0900

    캐릭터 메쉬 위치, 회전값 변경
    캡슐콜라이더와의 상대적인 위치, 회전 정상화

--- Source/ARPGProject/Character/ARCharacterBase.cpp
+++ Source/ARPGProject/Character/ARCharacterBase.cpp
@@ -2,8 +2,11 @@
 
 
 #include "ARCharacterBase.h"
+#include "Components/SkeletalMeshComponent.h"
 
 // Sets default values
 AARCharacterBase::AARCharacterBase()
 {
+	GetMesh()->SetRelativeLocation(FVector(0.f, 0.f,-80.f));
+	GetMesh()->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
 }
